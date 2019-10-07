266a980edbb64206f20c95a0b565817bcf319615
Fernando Silva <fehshuffle@gmail.com>
2019-10-07 18:02:39 -0300

    rota para authenticação feita

--- src/Controller/User.controller.js
+++ src/Controller/User.controller.js
@@ -1,18 +1,16 @@
-
 const User = require('../Model/User.model');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-
+// função para gerar jsonwebtoken que expira em 1 hora
 function generateToken( params = {}) {
   return jwt.sign(params, process.env.secret, {
-      expiresIn: 1800000,
+      expiresIn: '1h',
   });
 }
 module.exports = {
-  async store(req,res){
+  async register(req,res){
     try {
-      
       if(await User.findOne({ email: req.body.email})){
         return res.status(400).json({error: "Email already registered"})
       }
@@ -20,6 +18,7 @@ module.exports = {
       const hash = bcryptjs.hashSync(user.password,10)
       user.password = hash;
       await user.save();
+      // evitando que senha retorne ao registrar-se 
       user.password = undefined;
       return res.json({
         user, 
@@ -28,6 +27,27 @@ module.exports = {
     } catch (error) {
       return res.status(400).json({ error: "Registration fail" + error})
     }
-
+  },
+  async auth(req,res) {
+    try {
+      const { email, password} = req.body
+      const user = await User.findOne({ email }).select('+password');
+      if(!user){
+        return res.status(404).json({error: "User not found"})
+      }
+      if(!await bcryptjs.compare(password, user.password)){
+        return res.status(404).json({error: "Invalid email or password"})
+      }
+      const now = new Date()
+      user.password = undefined;
+      user.updatedAt = now
+      
+      return res.json({
+        user,
+        token: generateToken({id: user.id})})
+    } catch (error) {
+      return res.status(400).json({ error: "Something went wrong " + error})
+    }
   }
+  
 }
--- src/routes.js
+++ src/routes.js
@@ -2,5 +2,7 @@ const routes = require('express').Router();
 const UserController = require('./Controller/User.controller');
 
 
-routes.post('/users', UserController.store )
+routes.post('/users', UserController.register);
+routes.post('/auth', UserController.auth);
+
 module.exports = routes
